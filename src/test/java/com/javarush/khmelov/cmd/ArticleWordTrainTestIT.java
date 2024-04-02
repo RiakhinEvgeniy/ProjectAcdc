90b4af9e1f789aae79029cdd4db6c501533beb0e
Riakhin Evgeniy <apelles.odesskiy@gmail.com>
2024-04-02 09:49:26 +0200

    ADD tests for methods doGet classes ArticleTrain CheckArticle

--- src/test/java/com/javarush/khmelov/cmd/ArticleWordTrainTestIT.java
+++ src/test/java/com/javarush/khmelov/cmd/ArticleWordTrainTestIT.java
@@ -3,16 +3,14 @@ package com.javarush.khmelov.cmd;
 import com.javarush.khmelov.config.Winter;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
-import static org.mockito.Mockito.when;
+import org.mockito.Mockito;
 
 class ArticleWordTrainTestIT extends BaseIT {
 
     private final ArticleWordTrain articleWordTrain = Winter.find(ArticleWordTrain.class);
 
     @Test
-    void whenGetWord_thenWord() {
-        when(request.getParameter("id")).thenReturn("Pfanne");
+    void whenInvokeDoGet_thenReturnJspPage() {
         String jsp = articleWordTrain.doGet(request, response);
         Assertions.assertEquals("WEB-INF/article-word-train.jsp", jsp);
     }
--- src/test/java/com/javarush/khmelov/cmd/CheckArticleIT.java
+++ src/test/java/com/javarush/khmelov/cmd/CheckArticleIT.java
@@ -1,12 +1,20 @@
 package com.javarush.khmelov.cmd;
 
+import com.javarush.khmelov.config.Winter;
+import jakarta.servlet.ServletException;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class CheckArticleIT {
+class CheckArticleIT extends BaseIT {
 
+    CheckArticle checkArticle = Winter.find(CheckArticle.class);
     @Test
-    void doGet() {
+    void whenInvokeDoGet_thenReturnJspPage() throws ServletException, IOException {
+        String jsp = checkArticle.doGet(request, response);
+        Assertions.assertEquals("WEB-INF/check-article.jsp", jsp);
     }
 }
